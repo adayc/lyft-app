cabef1b6dd74231e7b5a23aae9a681576363d68b
adayc <adayc@hotmail.com>
2017-12-27 04:21:35 -0500

    Validando números ingresados

--- js/register.js
+++ js/register.js
@@ -1,7 +1,18 @@
 $(document).ready(function() {
+  // Deshabilitamos el boton al cargar la página
+  $('#btn-number').prop('disabled', true);
+// Obtenemos los codigos de ciudad de acuerdo al país
   $('#country a').on('click', function() {
     $('#select-country').html($(this).html() + '<span class="caret"></span>');  
     $('#code-country').val($(this).data('value'));     
-    
+  });
+  // Evento keyup en el texto para validar
+  $('#txt-phone').on('keyup', function() {
+    // Comparamos la longitud del texto y que sea númerico
+    if ($(this).val().length === 9 && $('#txt-phone').val().match(/^[0-9]+$/)) {
+      $('#btn-number').prop('disabled', false);
+    } else {
+      $('#btn-number').prop('disabled', true);
+    }
   });
 });

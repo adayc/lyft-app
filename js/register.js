69623a270b716285c85ead3809a1d5bade537dcd
adayc <adayc@hotmail.com>
2017-12-27 01:49:31 -0500

    Asignando value al text de acuerdo al pais

--- js/register.js
+++ js/register.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $('#country a').on('click', function() {
     $('#select-country').html($(this).html() + '<span class="caret"></span>');  
-    console.log(this);            
+    $('#code-country').val($(this).data('value'));     
     
   });
 });

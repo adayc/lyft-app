49aa6efd26aaecb9b4fb4d27f4d122415bfe26dd
adayc <adayc@hotmail.com>
2017-12-28 08:37:58 -0500

    Añadimos validate.html en dsktop

--- js/app2.js
+++
@@ -1,9 +0,0 @@
-$('#sizelist a').on('click', function() {
-  $('#sizevalue').val($(this).data('value'));
-});
-
-$('#country a').on('click', function() {
-  $('#select-country').html($(this).html() + '<span class="caret"></span>');  
-  console.log(this);            
-  /* ($(this).data('value'));*/
-});

1d8600d288d1a9f72ee9abe713ba6922828738cf
adayc <adayc@hotmail.com>
2017-12-28 04:51:33 -0500

    verify.js con jquery

--- js/register.js
+++ js/register.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
   $('#country a').on('click', function() {
     $('#select-country').html($(this).html() + '<span class="caret"></span>');
     $('#code-country').val($(this).data('value'));
+    window.localStorage.setItem('codeCountry', $(this).data('value'));
     $('#txt-phone').prop('disabled', false);
   });
   // Evento keyup en el texto para validar
